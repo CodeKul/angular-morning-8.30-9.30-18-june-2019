7856bdfa24a8fcfdf64edeff679417ebac0f7ff2
aniruddha <aniruddha.kudalkar@gmail.com>
2019-06-19 09:37:03 +0530

    ts : types

--- es6/scoping.js
+++ es6/scoping.js
@@ -1,8 +1,11 @@
 // what is let ?
 let nm = 'android'
+nm = true
+nm = 45
+nm = 45.89
 
 let bk = 10
-if(true) {
+if (true) {
     let bk = 20
 }
 console.log(bk)
---
+++ typescript/basics/types.ts
@@ -0,0 +1,19 @@
+let os: string = `codekul`
+//os = 78 error 
+let ver: number = 10
+
+let fn: () => void = function () {
+
+}
+fn = () => {
+
+}
+
+let obj: {} = {
+    match: true
+}
+
+function calc(num1: number, num2: number): string {
+    return `sa;d;vl`
+}
+
