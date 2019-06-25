b7a59d3cc7ca7f03674c940dc44f27935f8ea141
aniruddha <aniruddha.kudalkar@gmail.com>
2019-06-25 10:02:07 +0530

    component : basics and defination

--- angular/basic-app/src/app/app.component.ts
+++ angular/basic-app/src/app/app.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
 
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <h1> Welcome to codekul </h1> 
+    </div>
+  `,
+  styles: [`
+    h1 {
+      border : 1px solid red;
+    }
+  `]
 })
 export class AppComponent {
   title = 'codekul angular';
